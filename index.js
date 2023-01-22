c584c2e26ffb8ae14b3e91dff593dc3e03247263
Avery <averycaldwell7@gmail.com>
2023-01-22 15:58:53 -0800

    updated file structure

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // runs the application
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Import classes
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
