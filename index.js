26c016f42209dcc1fa800c7c4eedd333a595480e
Avery <averycaldwell7@gmail.com>
2023-01-22 20:15:49 -0800

    base styling added

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-// runs the application
-
 const inquirer = require('inquirer');
 const fs = require('fs');
+// const path = require('path');
 
 // Import classes
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const generateTeam = require('./src/template.js');
+
+// Function that pulls user input and inserts it into the HTML
+const writeDoc = (documentTemplate) => {
+  writeToFile('./team.html', documentTemplate);
+};
+
+// Function to initialize app
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      const documentTemplate = markdown(answers);
+      writeDoc(documentTemplate);
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        console.log(error);
+      } else {
+        console.log(error);
+      }
+    });
+}
+
+// Function call to initialize app
+init();
